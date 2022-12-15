d1bcc398e8de6bab5d5040b4b0725abd1f19125f
Luis Vargas <kluis0178@gmail.com>
2022-12-14 23:46:20 -0600

    estructura html + estilo basico + primera consulta

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('Hello World!');``
+import { API_KEY } from "./secrets.js";
+
+const BASE_URL = 'https://api.themoviedb.org/3'
+const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w300/';
+
+async function getTrendingMoviesPreview() {
+    
+    const movieContainer = document.getElementById('movie-container');
+    
+    const res = await fetch(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}`)
+    const data = await res.json()
+
+    const movies = data.results;
+    console.log(data.results)
+
+    movies.forEach(movie => {
+
+        const movieImgContainer = document.createElement('div');
+        movieImgContainer.classList.add('movie-img-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', `${IMG_BASE_URL}${movie.poster_path}`);
+
+        movieImgContainer.appendChild(movieImg);
+        movieContainer.appendChild(movieImgContainer);
+
+    });
+
+
+}
+
+getTrendingMoviesPreview()
---
+++ src/scripts/secrets.example.js
@@ -0,0 +1,3 @@
+// Crea porfavor un archivo secrets.js con 
+// tu API_KEY de themoviedb.org
+const API_KEY = '';
